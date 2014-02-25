08dc5ed15b8bb577781a850edcca18068326d0bd
afrubin <afrubin@uw.edu>
2014-02-25 15:23:45 +1100

    Fixed a bug and added some error handling.
    Fixed a typo (global instead of globals) and added some error handling
    when reading the JSON config file.
    
    Also converted tabs to spaces, causing all lines to show up as changed.

--- enrich/enrich.py
+++ enrich/enrich.py
@@ -12,27 +12,33 @@ from seqlib.overlap import OverlapSeqLib
 
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser()
-	parser.add_argument("config", help="JSON configuration file")
-	parser.add_argument("-l", "--log", metavar="file", help="path to new log file")
-	parser.add_argument("--no-plots", help="don't make plots", dest="plots", action="store_false", default=True)
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config", help="JSON configuration file")
+    parser.add_argument("-l", "--log", metavar="file", help="path to new log file")
+    parser.add_argument("--no-plots", help="don't make plots", dest="plots", action="store_false", default=True)
+    args = parser.parse_args()
 
-	config = json.load(open(args.config, "U"))
-	if config_check.is_experiment(config):
-		obj = Experiment(config)
-	elif config_check.is_selection(config):
-		obj = Selection(config)
-	#elif config_check.is_seqlib(config):
-		#obj = global()[config_check.seqlib_type(config)](config)
-	else:
-		raise EnrichError("Unrecognized .json config", "enrich.py")
+    try:
+        config = json.load(open(args.config, "U"))
+    except IOError:
+        raise EnrichError('Failed to open "%s"' % args.config, "enrich.py")
+    except ValueError:
+        raise EnrichError("Improperly formatted .json file", "enrich.py")
 
-	if args.log:
-		obj.enable_logging(open(args.log, "w"))
+    if config_check.is_experiment(config):
+        obj = Experiment(config)
+    elif config_check.is_selection(config):
+        obj = Selection(config)
+    elif config_check.is_seqlib(config):
+        obj = globals()[config_check.seqlib_type(config)](config)
+    else:
+        raise EnrichError("Unrecognized .json config", "enrich.py")
 
-	obj.calculate()
-	obj.save_data(config['output directory'], clear=False)
-	if args.plots:
-		obj.make_plots
-		
+    if args.log:
+        obj.enable_logging(open(args.log, "w"))
+
+    obj.calculate()
+    obj.save_data(config['output directory'], clear=False)
+    if args.plots:
+        obj.make_plots
+    
