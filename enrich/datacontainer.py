4b10092478eda576c622fb188fc9c02926651a29
mulescent <mulescent@dhcp8-15.gs.washington.edu>
2014-03-10 17:38:07 -0700

    Fixed spelling bug in datacontainer.py

--- enrich/datacontainer.py
+++ enrich/datacontainer.py
@@ -178,7 +178,7 @@ class DataContainer(object):
         for key in sorted(self.filter_stats, key=self.filter_stats.__getitem__, reverse=True):
             if key != 'total':
                 elements.append((DataContainer._filter_messages[key], self.filter_stats[key]))
-        elements.append(('total', self.filters_stats['total']))
+        elements.append(('total', self.filter_stats['total']))
         elements = ["\t".join(str(a) for a in e) for e in elements]
         logging.info("Filtered element statistics [%s]\n%s" % \
                 (self.name, "\n".join(elements)))
